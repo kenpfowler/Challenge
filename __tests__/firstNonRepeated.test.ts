884c38de54a38ab62bc49dc424cc968d47d6334d
Ken Fowler <development@kennyf.ca>
2023-07-16 16:56:18 -0400

    fix: pass correct arg to test case

--- __tests__/firstNonRepeated.test.ts
+++ __tests__/firstNonRepeated.test.ts
@@ -2,31 +2,26 @@ import { firstNonRepeated } from "..";
 
 test("Returns an empty string if the input is empty string", () => {
   const output = firstNonRepeated("");
-
   expect(output).toBe("");
 });
 
 test('firstNonRepeated("hello") => "h"', () => {
   const output = firstNonRepeated("hello");
-
   expect(output).toBe("h");
 });
 
 test('firstNonRepeated("aabcc") => "b"', () => {
   const output = firstNonRepeated("aabcc");
-
   expect(output).toBe("b");
 });
 
 test('firstNonRepeated("aaabcc") => "b"', () => {
-  const output = firstNonRepeated("aabcc");
-
+  const output = firstNonRepeated("aaabcc");
   expect(output).toBe("b");
 });
 
 test('firstNonRepeated("    ") => "   "', () => {
   const output = firstNonRepeated("   ");
-
   expect(output).toBe("");
 });
 
