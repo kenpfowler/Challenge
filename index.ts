ba073fae09b17390fcdece977d6f8520bd6d7a80
Ken Fowler <development@kennyf.ca>
2023-07-16 15:04:33 -0400

    feat: as recursive

--- index.ts
+++ index.ts
@@ -1,23 +1,16 @@
-export function firstNonRepeated(input: string) {
-  const inputArray = input.split("");
-  let currentIndex = 0;
-  let cache;
-
+export function firstNonRepeated(input: string, cache?: string) {
   if (!input.length) {
     return "";
   }
 
-  while (currentIndex < inputArray.length) {
-    const currentChar = inputArray[currentIndex];
-    const nextChar = inputArray[currentIndex + 1];
+  const current = input.charAt(0);
+  const next = input.charAt(1);
 
-    if (currentChar !== nextChar && currentChar !== cache) {
-      return currentChar;
-    }
-
-    cache = currentChar;
-    currentIndex++;
+  // base case
+  if (current !== next && current !== cache) {
+    return current;
   }
 
-  return "";
+  //recursive case
+  return firstNonRepeated(input.substring(1), current);
 }
